546863c5298c0d8094830332087a278741a72c95
Vishal Vidyarthi <91190476+MjCharlie@users.noreply.github.com>
2023-05-12 10:22:41 +0530

    Delete ecommerce directory

--- ecommerce/ecommerce/views.py
+++
@@ -1,5 +0,0 @@
-#code here
-from django.http import HttpResponse
-from django.shortcuts import render
-def main(request):
-    return render(request,"C:\\Users\\KARTA\\OneDrive\\Desktop\parent\\new project\ecommerce\\templates\\main.html")
